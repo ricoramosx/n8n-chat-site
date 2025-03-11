3d16ee5ca9835af2eaaa312b4b97222b6bc0cd06
Ricardo Ramos <76264325+ricoramosx@users.noreply.github.com>
2025-03-11 08:50:48 -0300

    Update chat-widget.js

--- chat-widget.js
+++ chat-widget.js
@@ -296,8 +296,8 @@
             welcomeText: '',
             responseTimeText: '',
             poweredBy: {
-                text: 'Powered by n8n',
-                link: 'https://n8n.partnerlinks.io/m8a94i19zhqq?utm_source=nocodecreative.io'
+                text: 'Criado por Elev Automações',
+                link: 'https://wa.me/5548991801805'
             }
         },
         style: {
